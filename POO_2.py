8b8863658a062797ea3934dcffdce322a984a5a1
Horacio Molinari <molinarih2011@gmail.com>
2024-10-16 13:23:06 -0300

    Modificado

--- POO_2.py
+++ POO_2.py
@@ -1,27 +1,38 @@
 #PILDORAS INFORMATICAS
-#Python tutorizado. POO V. Vídeo 33
+#Python tutorizado. POO V. Vídeo 33/34
 
 class Persona():
-    nombre = ""
-    apellido = ""
-    edad = 0
-    genero = "Sin definir"
+    __nombre = ""
+    __apellido = ""
+    __edad = 0
+    __genero = "Sin definir"
 
     def __init__(self, nombre, apellido, edad, genero):
-        self.nombre=nombre
-        self.apellido=apellido
-        self.edad=edad
-        self.genero=genero
+        self.__nombre=nombre
+        self.__apellido=apellido
+        self.__edad=edad
+        self.__genero=genero
+
+    def setEdad(self, laEdad):
+        if laEdad<0 or laEdad>100:
+            print("Error en la edad.")
+        else:
+            self.__edad=laEdad
+            return self.__edad    
 
     def habla(self):
 
-        return "La persona que se llama",self.nombre,"está hablando."
+        return "La persona que se llama",self.__nombre,"está hablando."
     
     def camina(self):
-        return "La persona que se llama",self.nombre,"está caminando."
+        return "La persona que se llama",self.__nombre,"está caminando."
     
     def getDatos(self):
-        return "Nombre: ",self.nombre,"Apellido: ",self.apellido,"Edad: ",self.edad,"Género: ",self.genero
+        return "Nombre: "+self.__nombre+" -Apellido: "+self.__apellido+" -Edad: "+str(self.__edad)+" -Género: "+self.__genero
     
 p1 = Persona("Horacio","Molinari",42,"masculino")
+p1.setEdad(-9)
+print(p1.getDatos())
+
+p1.nombre="Juan"
 print(p1.getDatos())
