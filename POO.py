36a3cf4267d8d53c8b756238a2b79b4e4c8ea79b
Horacio Molinari <molinarih2011@gmail.com>
2024-10-16 12:07:42 -0300

    Modificado

--- POO.py
+++ POO.py
@@ -1,15 +1,26 @@
 #PILDORAS INFORMÁTICAS  
-#Python tutorizado. POO III. Vídeo 31
+#Python tutorizado. POO III. Vídeo 31/32
 
 class Coche():
     ruedas = 4
     largoChasis = 260
     anchoChasis = 130
+    arrancado = False
 
     def arrancar(self):
-        pass
+        self.arrancado=True
+
+    def estadoCoche(self):
+        if(self.arrancado==True):
+            return "El coche está funcionando."
+        else:
+            return "El coche está parado"    
 
 mazda = Coche()    #Ejemplar de clase
 renault = Coche()
 
 print("Tu coche tiene",mazda.ruedas,"ruedas")
+
+renault.arrancar()
+
+print(renault.estadoCoche())
