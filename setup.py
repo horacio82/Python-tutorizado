5b7bac18f152df25f85a258358be1e83edbc1d28
Horacio Molinari <molinarih2011@gmail.com>
2024-10-27 15:12:34 -0300

    Nuevo

---
+++ setup.py
@@ -0,0 +1,12 @@
+#PILDORAS INFORMATICAS - Python tutorizado. Paquetes II. Vídeo 47
+
+from setuptools import setup
+
+setup(
+    name="Calculosbasicosmatematicos",
+    version="1.0",
+    description="Paquete para calculos",
+    author="Horacio Molinari",
+    author_email="molinarih2011@gmail.com",
+    url="",
+    packages=["moduloMatematico","moduloMatematico.calculosBasicos"])
