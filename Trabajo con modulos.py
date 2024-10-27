1e14925deb1a837711cca0ad789861dd24a2bce2
Horacio Molinari <molinarih2011@gmail.com>
2024-10-27 14:43:12 -0300

    Nuevo

--- Trabajo con modulos.py
+++ Trabajo con modulos.py
@@ -5,7 +5,10 @@
 #import Funciones_matematicas
 
 from moduloMatematico.calculosBasicos.Funciones_matematicas import *
+from moduloMatematico.otrosCalculos.PotenciaYredondeo import *
 
 sumar(20,15)
 restar(20,10)
 multiplicar(8,6)
+potencia(5,3)
+redondear(2.78)
--- moduloMatematico/otrosCalculos/PotenciaYredondeo.py
+++ moduloMatematico/otrosCalculos/PotenciaYredondeo.py
@@ -1,7 +1,7 @@
 #Python tutorizado. Paquetes I. Vídeo 46
 
-def potrncia(base, exponente):
+def potencia(base, exponente):
     print("El resultado de la potencia es:",base**exponente)
 
 def redondear(numero):
-    print("El número redondeado es:",round)(numero)    
+    print("El número redondeado es:",round(numero))    
