818b54d63d605e392c28a5a1da558784ab934b49
Horacio Molinari <molinarih2011@gmail.com>
2024-11-09 18:25:22 -0300

    modificado

--- Trabajo con crawlers.py
+++ Trabajo con crawlers.py
@@ -1,21 +1,66 @@
 #Pildoras informáticas - Python tutorizado. Crawlers II. Vídeo 50
-#Crawlers III. Vídeo 51
-import requests
-
-miReq = requests.get("https://www.pildorasinformaticas.es")
-
-print(miReq.status_code)
-print(miReq.headers)
-#print(miReq.text)
-
+#Crawlers III. Vídeo 51 - #Python tutorizado. Crawlers IV. Vídeo 52
+#Python tutorizado. Crawlers V. Vídeo 53 - #Python tutorizado. Crawlers VI. Vídeo 54 - #Python tutorizado. Crawlers VII. Vídeo 55
 from bs4 import BeautifulSoup
+import requests
+from urllib.parse import urljoin
+import time
 
-miDoc =requests.get("https://www.python.beispiel.programmierenlernen.io/")
+class PostCrawled():
+    def __init__(self, titulo, emoticono, contenido, imagen):
+        self.titulo=titulo
+        self.emoticono=emoticono
+        self.contenido=contenido
+        self.imagen=imagen
+        
+class PostExtractor():
 
-docFinal = BeautifulSoup(miDoc.text, "html.parser")
+    def extraeinfo(self):
 
-iconos = docFinal.select(".emoji")
-print(iconos[0].text)
+        urlBase = "https://www.python.beispiel.programmierenlernen.io/"
 
+        posts=[]
 
+        while urlBase!="":
 
+            miDoc = requests.get(urlBase)
+
+            docFinal = BeautifulSoup(miDoc.text, "html.parser")
+
+            time.sleep(2)
+
+            for card in docFinal.select(".card"):
+                titulo=card.select(".card-title span")[1].text
+                emoticono=card.select_one(".emoji").text
+                contenido=card.select_one(".card-text").text
+                imagen=urljoin(urlBase,card.select_one("img").attrs["src"])
+
+                crawled=PostCrawled(titulo,emoticono,contenido,imagen)
+                posts.append(crawled)
+
+            boton_siguiente = docFinal.select_one(".navigation .btn")
+
+            if boton_siguiente:
+                rutas_absolutas = urljoin(urlBase, boton_siguiente.attrs["href"])
+                urlBase = rutas_absolutas
+                print(rutas_absolutas)
+
+            else:
+                urlBase = ""
+            
+
+           
+        return posts
+
+unPost=PostExtractor()
+
+listaPost=unPost.extraeinfo()
+
+for elPost in listaPost:
+    print(elPost.emoticono)
+    print(elPost.titulo)
+    print(elPost.contenido)
+    print(elPost.imagen)
+    print()
+
+#print(listaPost)
