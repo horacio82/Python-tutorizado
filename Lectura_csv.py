72ec4b0fdaadd1e80d33c8198fcca6bff3a5d3a0
Horacio Molinari <molinarih2011@gmail.com>
2024-11-13 17:36:17 -0300

    Nuevo

---
+++ Lectura_csv.py
@@ -0,0 +1,11 @@
+#PILDORAS INFORMÁTICAS
+#Python tutorizado. Crawlers IX. Vídeo 57
+import csv
+
+with open('posts.csv', 'r', newline='', encoding="utf-8") as csvfile:
+    postreader = csv.reader(csvfile, delimiter=';',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    
+    for fila in postreader:
+        print(', '.join(fila))
+        print()
