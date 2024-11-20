8e282e73e4ceba36f844878081cbbd3dcba9983a
Horacio Molinari <molinarih2011@gmail.com>
2024-11-20 17:20:06 -0300

    Comentado

--- Trabajo con modulos.py
+++ Trabajo con modulos.py
@@ -4,11 +4,18 @@
 
 #import Funciones_matematicas
 
+#Importación de módulos y funciones
 from moduloMatematico.calculosBasicos.Funciones_matematicas import *
 from moduloMatematico.otrosCalculos.PotenciaYredondeo import *
+'''Estas líneas importan todas las funciones definidas en los módulos Funciones_matematicas y PotenciaYredondeo, 
+ubicados en los directorios calculosBasicos y otrosCalculos respectivamente. La sintaxis import * indica que todas las 
+funciones disponibles en esos módulos serán importadas.'''
 
+#Uso de las funciones importadas:
 sumar(20,15)
 restar(20,10)
 multiplicar(8,6)
 potencia(5,3)
 redondear(2.78)
+
+'''Para que este código funcione correctamente, necesitas tener definidos y disponibles esos módulos (moduloMatematico.calculosBasicos.Funciones_matematicas y moduloMatematico.otrosCalculos.PotenciaYredondeo) en tu entorno de Python.'''
