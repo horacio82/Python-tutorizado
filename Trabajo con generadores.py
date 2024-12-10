bcb7064afad7e8fb7028959134a614697ab0bc6a
Horacio Molinari <molinarih2011@gmail.com>
2024-12-10 19:19:03 -0300

    Comentado

--- Trabajo con generadores.py
+++ Trabajo con generadores.py
@@ -6,26 +6,33 @@ def generarPares(limite):
 
     num=1
 
-    numerosPares=[]
+    numerosPares=[]#Crea una lista vacía para guardar los pares.
 
-    while num<limite:
-        numerosPares.append(num*2)
-        num=num+1
+    while num<limite:#Mientras que num sea menor que limite.
+        numerosPares.append(num*2)#multiplica num por 2 y agrega el resultado a la lista numerosPares.
+        num=num+1 #incrementa num en 1 en cada iteración.
 
-    return numerosPares
+    return numerosPares #devuelve la lista numerosPares al final del bucle.
 
 print(generarPares(6))    
 
 
-#Función con generador:
+#Función con generador: Similar a la función anterior, pero en lugar de usar una lista para almacenar los resultados, 
+# se utiliza yield para generar los valores uno a uno.
 def generarPares1(limit):
     numero=1
 
     while numero<limit:
-        yield numero*2
+        yield numero*2 #produce un número par cada vez que la función es llamada, permitiendo ahorrar memoria 
+        #ya que no se almacena toda la lista en memoria.
         numero=numero+1
 
-sucesionPares=generarPares1(6)
+sucesionPares=generarPares1(6) #Creación de un objeto generador
 
-for i in sucesionPares:
+for i in sucesionPares: #Itera sobre los valores generados por generarPares1 e imprime cada uno.
     print("->",i)
+
+#En resumen, ambos métodos tienen la misma finalidad de generar números pares hasta un límite dado. 
+# La primera función utiliza una lista para almacenar y devolver todos los números a la vez, 
+# mientras que la segunda utiliza un generador (yield), produciendo los números uno por uno bajo demanda, 
+# lo que es más eficiente en términos de memoria.    
