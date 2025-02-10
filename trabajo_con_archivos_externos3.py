36eb1e7cb28db5dc54552e6b76444532f7c142f5
Horacio Molinari <molinarih2011@gmail.com>
2025-02-10 11:20:43 -0300

    nuevo

---
+++ trabajo_con_archivos_externos3.py
@@ -0,0 +1,10 @@
+#PILDORAS INFORMATICAS
+#Python tutorizado. Archivos externos III. Vídeo 62
+
+import os #importamos el modulo os
+
+
+#os.makedirs("PruebaDirectorio") #creamos un directorio
+os.chdir("PruebaDirectorio") #cambiamos de directorio
+
+print(os.getcwd()) #mostramos el directorio de trabajo
