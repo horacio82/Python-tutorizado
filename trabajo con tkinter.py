53749a2713b6f6c5fa13f7fd89d5dd14a358757f
Horacio Molinari <molinarih2011@gmail.com>
2025-02-13 12:17:18 -0300

    modificado

--- trabajo con tkinter.py
+++ trabajo con tkinter.py
@@ -10,9 +10,19 @@ raiz.title("Primera ventana") #titulo de la ventana
 raiz.resizable(1,1) #ancho y alto de la ventana redimensionables
 raiz.iconbitmap("ACOrigins_0.ico") #icono de la ventana
 
-raiz.geometry("650x350") #dimensiones de la ventana
+#raiz.geometry("650x350") #dimensiones de la ventana
 
 raiz.config(bg="blue") #color de fondo de la ventana
+
+miFrame=Frame() #creamos un frame
+
+#miFrame.pack(side="right", anchor="n") #empaquetamos el frame en la raiz
+
+miFrame.config(bg="red") #color de fondo del frame
+
+miFrame.config(width="650", height="350") #dimensiones del frame
+
+miFrame.pack(fill="x") #expandir el frame en el eje x
 
 raiz.mainloop() #bucle infinito que mantiene la ventana abierta
 
