bf98ca04e06e914d337ad8ef86ed815613ad042f
Horacio Molinari <molinarih2011@gmail.com>
2025-02-19 16:28:45 -0300

    nuevo

--- trabajo con entry.py
+++ trabajo con entry.py
@@ -1,44 +1,59 @@
 #PILDORAS INFORMATICAS  
-#Python tutorizado. Interfaces gráficas. Tkinter IV. Vídeo 67
+#Python tutorizado. Interfaces gráficas. Tkinter IV, V Vídeo 67, 68
 
 from tkinter import *
 
-raiz=Tk() #creamos la raiz
+# Crear la ventana principal
+raiz = Tk()
 
-miFrame=Frame(raiz, width=1000, height=550) #creamos un frame
-miFrame.pack() #empaquetamos el frame
+# Crear un frame
+miFrame = Frame=Frame(raiz, width=1000, height=550)
+miFrame.pack()
 
-#---------------------creamos cuadros de texto-----------------------------------
-cuadroTextoNombre=Entry(miFrame) #creamos un cuadro de texto
-cuadroTextoNombre.grid(row=0, column=1,) #posicionamos el cuadro de texto
-cuadroTextoNombre.config(fg="red", justify="center") #configuramos el cuadro de texto
+# Cuadros de texto
+cuadroTextoNombre = Entry(miFrame)
+cuadroTextoNombre.grid(row=0, column=1, padx=15, pady=15)
+cuadroTextoNombre.config(fg="red", justify="center")
 
-cuadroTextoApellido=Entry(miFrame) #creamos un cuadro de texto
-cuadroTextoApellido.grid(row=1, column=1) #posicionamos el cuadro de texto
+cuadroTextoApellido = Entry(miFrame)
+cuadroTextoApellido.grid(row=1, column=1, padx=15, pady=15)
 
-cuadroTextoDireccion=Entry(miFrame) #creamos un cuadro de texto
-cuadroTextoDireccion.grid(row=2, column=1) #posicionamos el cuadro de texto
+cuadroTextoContra = Entry(miFrame)
+cuadroTextoContra.grid(row=2, column=1, padx=15, pady=15)
+cuadroTextoContra.config(show="*")
 
-cuadroTextoMail=Entry(miFrame) #creamos un cuadro de texto
-cuadroTextoMail.grid(row=3, column=1) #posicionamos el cuadro de texto
+cuadroTextoDireccion = Entry(miFrame)
+cuadroTextoDireccion.grid(row=3, column=1, padx=15, pady=15)
+
+cuadroTextoMail = Entry(miFrame)
+cuadroTextoMail.grid(row=4, column=1, padx=15, pady=15)
+
+cuadroTextoOpiniones = Text(miFrame, width=15, height=10)
+cuadroTextoOpiniones.grid(row=5, column=1, padx=15, pady=15)
+
+# Etiquetas------------------------------------------------
+nombreLabelNombre = Label(miFrame, text="Nombre:")
+nombreLabelNombre.grid(row=0, column=0, sticky="w")
+
+nombreLabelApellido = Label(miFrame, text="Apellido:")
+nombreLabelApellido.grid(row=1, column=0, sticky="w")
+
+nombreLabelContra = Label(miFrame, text="Contraseña:")
+nombreLabelContra.grid(row=2, column=0, sticky="w")
+
+nombreLabelDireccion = Label(miFrame, text="Dirección:")
+nombreLabelDireccion.grid(row=3, column=0, sticky="w")
+
+nombreLabelMail = Label(miFrame, text="Mail:")
+nombreLabelMail.grid(row=4, column=0, sticky="w")
+
+nombreLabelComentarios = Label(miFrame, text="Comentarios:")
+nombreLabelComentarios.grid(row=5, column=0, sticky="w")
+
+# Botón
+botonEnviar = Button(raiz, text="Enviar")
+botonEnviar.pack()
 
 
-#----------------------creamos etiquetas-----------------------------------------
-nombreLabelNombre=Label(miFrame, text="Nombre:") #creamos una etiqueta
-nombreLabelNombre.grid(row=0, column=0, sticky="w") #posicionamos la etiqueta
-
-nombreLabelApellido=Label(miFrame, text="Apellido:") #creamos una etiqueta
-nombreLabelApellido.grid(row=1, column=0, sticky="w") #posicionamos la etiqueta
-
-nombreLabelDireccion=Label(miFrame, text="Dirección:") #creamos una etiqueta
-nombreLabelDireccion.grid(row=2, column=0, sticky="w") #posicionamos la etiqueta
-
-nombreLabelMail=Label(miFrame, text="Mail:") #creamos una etiqueta
-nombreLabelMail.grid(row=3, column=0, sticky="w") #posicionamos la etiqueta
-
-
-
-
-
-
-raiz.mainloop() #bucle infinito para que se ejecute la ventana
+# Iniciar el bucle principal de Tkinter
+raiz.mainloop()
