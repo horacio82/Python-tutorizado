e8b835fef2209907b3c999ea8b4e51ee1282ad4b
Horacio Molinari <molinarih2011@gmail.com>
2025-04-01 10:54:22 -0300

    modificado

--- trabajo con fun decoradoras.py
+++ trabajo con fun decoradoras.py
@@ -1,20 +1,25 @@
 #PILDORAS INFORMATICAS
-#Python tutorizado. Decoradores I. Vídeo 91
+#Python tutorizado. Decoradores I, II. Vídeo 91, 92
 
 def funcion_decoradora(funcion_parametro):# Decorador
-    def funcion_interior():
+    def funcion_interior(*args, **kwargs): # *args y **kwargs permiten pasar un número variable de argumentos
         print("A continuación voy a realizar un cálculo")
-        funcion_parametro()
+        funcion_parametro(*args, **kwargs)
         print("He terminado el cálculo")
     return funcion_interior
 
 @funcion_decoradora # Decorador
-def suma():
-    print(25+30)
-    
-@funcion_decoradora
-def resta():
-    print(80-25)
+def suma(num1, num2):
+    print(num1+num2)
 
-suma()
-resta()        
+@funcion_decoradora
+def resta(num1, num2):
+    print(num1-num2)
+
+@funcion_decoradora
+def potencia(base, exponente):
+    print(pow(base, exponente))
+
+suma(5,7)
+resta(20,5)        
+potencia(base=5,exponente=3)
