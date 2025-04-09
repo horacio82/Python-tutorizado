cc4e3a91a8cab8fec8e7b2a5ba22cedbcd82fcfe
Horacio Molinari <molinarih2011@gmail.com>
2025-04-09 08:08:25 -0300

    modificado

--- Trabajo con BBDD/practica_datos.py
+++ Trabajo con BBDD/practica_datos.py
@@ -1,5 +1,5 @@
 #PILDORAS INFORMATICAS
-#Python tutorizado. BBDD I. Vídeo 93
+#Python tutorizado. BBDD I,II. Vídeo 93,94
 
 import sqlite3 # Importar la librería sqlite3 para trabajar con bases de datos SQLite
 
@@ -9,15 +9,30 @@ mi_cursor = mi_conexion.cursor() # Crear un cursor para ejecutar comandos SQL
 
 #mi_cursor.execute("CREATE TABLE PRODUCTOS (ARTICULO VARCHAR (50), PRECIO INTEGER, SECCION VARCHAR (20))") # Crear una tabla con tres columnas: ARTICULO, PRECIO y SECCION
 
-mi_cursor.execute("INSERT INTO PRODUCTOS VALUES ('Camiseta', 25, 'Moda')") # Insertar un registro en la tabla
+#mi_cursor.execute("INSERT INTO PRODUCTOS VALUES ('Camiseta', 25, 'Moda')") # Insertar un registro en la tabla
 
-mi_conexion.commit() # Guardar los cambios en la base de datos
+'''muchosProductos = [
+    ('Pantalón', 20, 'Moda'), # Crear una lista de tuplas con varios registros
+    ('Jersey', 30, 'Moda'),
+    ('Chaqueta', 50, 'Moda'),
+    ('Zapatos', 80, 'Calzado'),
+    ('Botas', 100, 'Calzado')
+]'''
 
+#mi_cursor.executemany("INSERT INTO PRODUCTOS VALUES (?,?,?)", muchosProductos) # Insertar varios registros a la vez
 
+mi_cursor.execute("SELECT * FROM PRODUCTOS") # Seleccionar todos los registros de la tabla
+muchosProductos = mi_cursor.fetchall() # Obtener todos los registros seleccionados
 
+print(muchosProductos) # Imprimir los registros obtenidos
 
+for p in muchosProductos: # Recorrer los registros obtenidos
+    print("Artículo:", p[0]) # Imprimir el artículo
+    print("Precio:", p[1]) # Imprimir el precio
+    print("Sección:", p[2]) # Imprimir la sección
+    print("-------------------") # Separador
 
-
+#mi_conexion.commit() # Guardar los cambios en la base de datos
 
 mi_cursor.close() # Cerrar el cursor
 
