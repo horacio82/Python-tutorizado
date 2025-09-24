7983f9635f4d5638dab2c768b083d4b1010cfe37
Horacio Molinari <molinarih2011@gmail.com>
2025-09-24 06:25:39 -0300

    nuevo

---
+++ Trabajo con listas.py
@@ -0,0 +1,19 @@
+#PILDORAS INFORMATICAS
+#Python tutorizado. Listas. Vídeo 6
+"""En este vídeo vemos las listas. Las listas son similares a los array de otros lenguajes y son unas estructuras muy utilizadas a la hora de programar"""
+
+#Creando una lista:
+trabajadores = ["Ana","María","Antonio","Miguel","Juan"]
+print(trabajadores)
+
+#imprime la longitud de la lista:
+print(len(trabajadores))
+
+#imprime el elemento de tal posición:
+print(trabajadores[2])
+
+#imprime el último elemento de la lista:
+print(trabajadores[-1])
+
+#imprime los 3 primeros elementos de la lista:
+print(trabajadores[0:3])
