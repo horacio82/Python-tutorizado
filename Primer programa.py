86a7e86c0307a0d05fba1f097cc3937736999196
Horacio Molinari <molinarih2011@gmail.com>
2025-09-24 05:36:24 -0300

    nuevos

--- Primer programa.py
+++ Primer programa.py
@@ -1,6 +1,10 @@
 #PILDORAS INFORMATICAS
-#Python tutorizado. Primer programa. Vídeo 2
+#Python tutorizado. Primer programa. Vídeo 2, 4
 
 mensaje = "Hola mundo"
+
+print(mensaje)
+
+mensaje = "Adios Python"
 
 print(mensaje)
