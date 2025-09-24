8e26c382474a16212357ee9c34ff7aab0df38fe8
Horacio Molinari <molinarih2011@gmail.com>
2025-09-24 06:03:46 -0300

    modificado

--- Primer programa.py
+++ Primer programa.py
@@ -1,10 +1,26 @@
 #PILDORAS INFORMATICAS
-#Python tutorizado. Primer programa. Vídeo 2, 4
+#Python tutorizado. Primer programa. Vídeo 2, 4, 5
 
+#VARIABLES:
 mensaje = "Hola mundo"
-
 print(mensaje)
 
-mensaje = "Adios Python"
+mensaje1 = "estoy aprendiendo Python."
 
-print(mensaje)
+edad = 43
+
+
+#CONSTANTES: se escriben en mayúscula.
+NUMERO1 = 8
+
+#Concatenar: string + una variable.
+print("Hola, " + mensaje1)
+
+print("Hola, mi nombre es Horacio y mi edad es de " + str(edad))
+
+#------------------------------------------------------
+
+sueldo = 1500000
+servicios = 150000
+
+print("El descuento queda en: " + str(sueldo-servicios))
