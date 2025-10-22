9466c4470eb0f62e656bb4b026e9969b8ff4b304
Horacio Molinari <molinarih2011@gmail.com>
2025-10-22 10:13:54 -0300

    nuevo

---
+++ Trabajo con condicionales_2.py
@@ -0,0 +1,22 @@
+#PILDROAS INFORMATICAS
+#Python tutorizado. Control de flujo. Operadores NOT, IN. Vídeo 17
+
+
+trabajadores = ["Paula","Manuel","Pedro","Ana","Sandra"]
+
+if "Pedro" in trabajadores:
+    print("Sí, Pedro está en la lista.")
+
+else:
+    print("Pedro no está en la lista.")
+
+#-------------------------------------------------------------------
+
+
+lenguajes_Trim1 = "Java","Python","PHP","C#","HTML","JavaScript"
+
+if "SQL" not in lenguajes_Trim1:
+    print("Falta SQL en la lista..")
+
+else:
+    print("SQL está en la lista.")
