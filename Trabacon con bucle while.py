a86113baab2073d3290a04079a8039c1c38fe633
Horacio Molinari <molinarih2011@gmail.com>
2025-10-28 10:59:47 -0300

    modificado

--- Trabacon con bucle while.py
+++ Trabacon con bucle while.py
@@ -1,6 +1,7 @@
 #PILDORAS INFORMATICAS
-#Python tutorizado. Control de flujo. Bucle While I. Vídeo 18
+#Python tutorizado. Control de flujo. Bucle While I, II - Vídeo 18, 19.
 
+import math 
 
 contador = 0
 
@@ -14,9 +15,34 @@ print("Hemos salido del bucle.")
 
 edad = int(input("Introduce tu edad: "))
 
-while edad < 0:
-    print("Has introducido una edad negativa o no válida.")
+while edad < 0 or edad > 100:
+    print("Has introducido una edad no válida.")
     edad = int(input("Introduce tu edad: "))
 
 print("Gracias, puedes pasar.")
 print("Edad del usuario "+str(edad))
+
+#----------------------------------------------------------------------
+
+print("Este programa halla la raíz cuadrada de un número.")
+
+numero = int(input("Introduce un número: "))
+
+intentos = 1
+
+while numero<0:
+    print("El valor numérico no puede ser negativo.")
+
+    numero = int(input("Introduce un número: "))
+
+    intentos=intentos+1
+
+    if intentos==5:
+
+        break
+
+if intentos==5:
+    print("Lo siento, no puedo realizar el cálculo:")
+else:
+    print(math.isqrt(numero))
+
