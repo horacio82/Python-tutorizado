82bc3f1593d27e97c9ccafd679906fca0b580985
Horacio Molinari <molinarih2011@gmail.com>
2025-11-12 10:04:02 -0300

    nuevo

---
+++ Diccionarios y bucles.py
@@ -0,0 +1,9 @@
+#PILDORAS INFORMATICAS
+#Python tutorizado. Bucles y diccionarios. Vídeo 22
+
+
+capitales={"China":"Pekín","India":"Nueva Delhi","Indonesia":"Yakarta","Japón":"Tokio","Bangladesh":"Dacca"}
+
+for clave, valor in capitales.items():
+
+    print(clave + "->" + valor)
