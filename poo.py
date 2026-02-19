e7dcd2e87bb22f6021182137be4682a3f05b897f
Horacio Molinari <molinarih2011@gmail.com>
2026-02-19 08:13:52 -0300

    nuevo

---
+++ poo.py
@@ -0,0 +1,17 @@
+#PILDORAS INFORMATICAS
+#Python tutorizado. POO III. Vídeo 31
+
+class Coche():
+    ruedas=4
+    largoChasis=260
+    anchoChasis=130
+
+
+    def arrancar(self):
+        pass
+
+mazda=Coche()  #ejemplar de clase
+
+renault=Coche()
+
+print("Tu coche tiene",mazda.ruedas,"ruedas.")
