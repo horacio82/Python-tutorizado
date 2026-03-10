dfb6df1d4967be242f59df847316f8295f31f225
Horacio Molinari <molinarih2011@gmail.com>
2026-03-10 10:47:02 -0300

    modificado

--- Trabajo metodos especiales.py
+++ Trabajo metodos especiales.py
@@ -1,6 +1,8 @@
 #PILDORAS INFORMATICAS
-#Python tutorizado. Métodos útiles y especiales II. Vídeo 42
+#Python tutorizado. Métodos útiles y especiales II Y III. Vídeo 42, 43
 
+
+import datetime
 class Persona():
 
     almacen_datos=[]
@@ -19,3 +21,29 @@ class Persona():
 
 p1=Persona("Horacio","Molinari",44, 1.500, "Murata S.A.")
 print(p1)         
+
+#------------------------video 43-----------------------------
+
+hoy = datetime.date.today()
+print(str(hoy))
+print(repr(hoy))
+
+#------------------------video 43---------------------------
+
+class Agenda():
+
+    def __init__(self):
+        self.miAgenda={}
+
+    def agregarPersonas(self, nombre, telefono):
+
+        self.miAgenda[nombre]=telefono
+    def __len__(self):
+        return len(self.miAgenda)
+    
+
+agendaPersonal = Agenda()
+agendaPersonal.agregarPersonas("Horacio","1133915281")
+agendaPersonal.agregarPersonas("Natalia","1151791518")
+
+print(len(agendaPersonal))
