8a336697bc537841b007fb7aa75a254f753f8a8e
Horacio Molinari <molinarih2011@gmail.com>
2026-03-12 12:19:48 -0300

    nuevos

---
+++ ModuloMatematico/funciones_matematicas.py
@@ -0,0 +1,12 @@
+#PILDORAS INFORMATICAS
+#Python tutorizado. Módulos II. Vídeo 45
+
+
+def sumar(num1,num2):
+    print("El resultado de la suma es: ",num1+num2)
+
+def restar(num1,num2):
+    print("El resultado de la resta es: ",num1-num2)    
+
+def multiplicar(num1,num2):
+    print("El resultade de la multiplicación es: ",num1*num2)    
---
+++ Trabajo con modulos.py
@@ -0,0 +1,8 @@
+#PILDORAS INFORMATICAS
+#Python tutorizado. Módulos II. Vídeo 45
+
+from ModuloMatematico.funciones_matematicas import *
+
+sumar(8,2)
+restar(15,8)
+multiplicar(9,8)
