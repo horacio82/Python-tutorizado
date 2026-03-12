a752bd6f271a0f7a0248ce7554fb28cc16529ab6
Horacio Molinari <molinarih2011@gmail.com>
2026-03-12 12:39:33 -0300

    modificado

--- Trabajo con modulos.py
+++ Trabajo con modulos.py
@@ -1,8 +1,13 @@
 #PILDORAS INFORMATICAS
 #Python tutorizado. Módulos II. Vídeo 45
+#Python tutorizado. Paquetes I. Vídeo 46
+
 
 from ModuloMatematico.funciones_matematicas import *
-
+from ModuloMatematico.otrosCalculos.potenciaYredondeo import *
 sumar(8,2)
 restar(15,8)
 multiplicar(9,8)
+
+potencia(5,3)
+redondear(2.78)
