f9db33c7f9c8b5271823af732f2d00f2643cde49
Horacio Molinari <molinarih2011@gmail.com>
2026-03-12 12:36:01 -0300

    modificado

---
+++ ModuloMatematico/otrosCalculos/potenciaYredondeo.py
@@ -0,0 +1,10 @@
+#PILDORAS INFORMATICAS
+#Python tutorizado. Paquetes I. Vídeo 46
+
+def potencia(base,exponente):
+    print("El resultado de la potencia es: ",base**exponente)
+
+def redondear(numero):
+    print("El número redondeado es: ",round(numero))
+    
+        
