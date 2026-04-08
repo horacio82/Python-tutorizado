ecbf6015ba99f97543b94e4248475525974836d8
Horacio Molinari <molinarih2011@gmail.com>
2026-04-08 05:51:55 -0300

    modificado

---
+++ setup.py
@@ -0,0 +1,14 @@
+#PILDORAS INFORMATICAS
+#Python tutorizado. Paquetes II. Vídeo 47
+
+
+from setuptools import setup
+
+setup(
+    name="ModuloMatematico",
+    version="1.0",
+    description="Un módulo matemático con funciones básicas y cálculos adicionales",
+    author="Horacio Molinari",
+    author_email="molinarih2011@gmail.com",
+    url="https://github.com/horaciomolinari/ModuloMatematico",
+    packages=["ModuloMatematico", "ModuloMatematico.funciones_matematicas", "ModuloMatematico.otrosCalculos"])
