608ff1c5159b7c3c423d504045bf68d2b5cb56ea
Horacio Molinari <molinarih2011@gmail.com>
2026-04-14 10:04:47 -0300

    modificado

--- Trabajo con crawlers.py
+++ Trabajo con crawlers.py
@@ -6,27 +6,11 @@ import requests
 from bs4 import BeautifulSoup
 
 
-#miReq=requests.get("https://www.movistar.es/")
-#print(miReq.status_code)
-#print(miReq.headers)
-#print(miReq.text)
+miDoc=requests.get("https://www.youtube.com/watch?v=X4d4barv1fA")
 
-miDoc="""
+docFinal=BeautifulSoup(miDoc.text,"html.parser")
 
-    <html>
-        <body>
-            <p>Hola, soy un párrafo</p>
-            <p>Hola, soy otro párrafo</p>
-            <a>Hola, soy un enlace</a>
-        </body>
-    </html>
+docFinal.select(".emoji")
 
+print(docFinal.select(".emoji"))
 
-"""
-
-docFinal=BeautifulSoup(miDoc,"html.parser")
-
-for parrafos in docFinal.find_all("p"):
-    print(parrafos.text)
-
-print(docFinal.find_all("p")) 
