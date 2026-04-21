ce3835408853acb7305c47aa3fd5cdf6a047a273
Horacio Molinari <molinarih2011@gmail.com>
2026-04-21 09:33:02 -0300

    modificado

--- Trabajo con crawlers.py
+++ Trabajo con crawlers.py
@@ -1,16 +1,49 @@
-#Python tutorizado. Crawlers II, III, IV - Vídeo 50, 51, 52
+#Python tutorizado. Crawlers II, III, IV, V - Vídeo 50, 51, 52, 53, 
 #vemos cómo extraer información de una página web desde Python
 
 
 import requests
 from bs4 import BeautifulSoup
 
+class PostCrawled():
 
-miDoc=requests.get("https://www.youtube.com/watch?v=X4d4barv1fA")
+    def __init__(self, titulo, emoticono, contenido, imagen):
 
-docFinal=BeautifulSoup(miDoc.text,"html.parser")
+        self.titulo=titulo
+        self.emoticono=emoticono
+        self.contenido=contenido
+        self.imagen=imagen
+        
+class PostExtractor():
 
-docFinal.select(".emoji")
 
-print(docFinal.select(".emoji"))
+    def extraer(self):
 
+        miDoc=requests.get("https://www.youtube.com/watch?v=X4d4barv1fA")
+
+        docFinal=BeautifulSoup(miDoc.text,"html.parser")
+
+        posts=[]
+
+        for card in docFinal.select(".card"):
+            titulo=card.select(".card-title span")[1].text
+            emoticono=card.select_one(".emoji").text
+            contenido=card.select_one(".card-text").text
+            imagen=card.select_one("img").attrs["src"]
+
+            crawled=PostCrawled(titulo, emoticono, contenido, imagen)
+            posts.append(crawled)
+        return posts
+    
+unPost=PostExtractor()
+
+listaPosts=unPost.extraer()
+
+for post in listaPosts:
+    print(post.emoticono)
+    print(post.titulo)
+    print(post.contenido)
+    print(post.imagen)
+    print()
+    
+#print(listaPosts)
