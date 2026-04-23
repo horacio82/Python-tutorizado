2d53b49856105a75400d06f20fdc9ab2c5aba473
Horacio Molinari <molinarih2011@gmail.com>
2026-04-23 07:55:12 -0300

    modificado

--- Trabajo con crawlers.py
+++ Trabajo con crawlers.py
@@ -1,10 +1,11 @@
-#Python tutorizado. Crawlers II, III, IV, V, VI - Vídeo 50, 51, 52, 53, 54
+#Python tutorizado. Crawlers II, III, IV, V, VI, VII - Vídeo 50, 51, 52, 53, 54, 55
 #vemos cómo extraer información de una página web desde Python
 
 
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
+import time
 
 class PostCrawled():
 
@@ -20,21 +21,38 @@ class PostExtractor():
 
     def extraer(self):
 
-        urlBase="https://www.youtube.com/watch?v=X4d4barv1fA"
-        miDoc=requests.get(urlBase)
-
-        docFinal=BeautifulSoup(miDoc.text,"html.parser")
+        urlBase="http://python.beispiel.programmierenlernen.io/index.php"
 
         posts=[]
 
-        for card in docFinal.select(".card"):
-            titulo=card.select(".card-title span")[1].text
-            emoticono=card.select_one(".emoji").text
-            contenido=card.select_one(".card-text").text
-            imagen=urljoin(urlBase, card.select_one("img").attrs["src"])
 
-            crawled=PostCrawled(titulo, emoticono, contenido, imagen)
-            posts.append(crawled)
+        while urlBase!="":
+
+            miDoc=requests.get(urlBase)
+
+            docFinal=BeautifulSoup(miDoc.text,"html.parser")
+
+            time.sleep(2)
+
+            for card in docFinal.select(".card"):
+                titulo=card.select(".card-title span")[1].text
+                emoticono=card.select_one(".emoji").text
+                contenido=card.select_one(".card-text").text
+                imagen=urljoin(urlBase, card.select_one("img").attrs["src"])
+
+                crawled=PostCrawled(titulo, emoticono, contenido, imagen)
+                posts.append(crawled)
+
+            boton_siguiente=docFinal.select_one(".navigation .btn")
+
+            if boton_siguiente:
+                rutas_absolutas=urljoin(urlBase, boton_siguiente.attrs["href"])
+                urlBase=rutas_absolutas
+                print(rutas_absolutas)    
+            else:
+                urlBase=""
+          
+
         return posts
     
 unPost=PostExtractor()
