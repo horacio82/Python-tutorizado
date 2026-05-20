17bce1ef127fd6c2f4d88304a15dd8ef22bdbf3b
Horacio Molinari <molinarih2011@gmail.com>
2026-05-20 12:21:02 -0300

    modificado

--- Archivos externos.py
+++ Archivos externos.py
@@ -1,14 +1,20 @@
 #PILDORAS INFORMATICAS
-#Python tutorizado. Archivos externos I, II. Vídeo 60, 61
+#Python tutorizado. Archivos externos I, II, III. Vídeo 60, 61, 62
 
 '''Comenzamos a ver en este vídeo el acceso externo a ficheros desde Python. El acceso externo se utiliza para almacenar y leer información en archivos externos. La ventaja es que la información se puede almacenar de forma permanente'''
 
 from io import open
 
-archivo_externo = open("primerArchivo.txt", "r") #Abrimos el archivo externo en modo lectura
+archivo_externo = open("primerArchivo.txt", "r+") #Abrimos el archivo en modo lectura y escritura. Si el archivo no existe, se creará automáticamente
 
-print(archivo_externo.read()) #Leemos el contenido del archivo externo
+lista_archivo = archivo_externo.readlines() #Leemos el contenido del archivo y lo guardamos en una lista
 
-archivo_externo.seek(6) #Volvemos a colocar el cursor de lectura al principio del archivo
+lista_archivo[1] = "Esta línea ha sido modificada\n" #Modificamos el primer elemento de la lista
 
-print(archivo_externo.read()) #Si intentamos leer el archivo externo de nuevo, no se muestra nada porque el cursor de lectura ya está al final del archivo
+archivo_externo.seek(0) #Volvemos al principio del archivo para escribir desde el inicio
+
+archivo_externo.writelines(lista_archivo) #Escribimos la lista modificada en el archivo
+
+archivo_externo.close() #Cerramos el archivo para guardar los cambios
+
+print(lista_archivo) #Mostramos el contenido del archivo
---
+++ Archivos externos2.py
@@ -0,0 +1,6 @@
+#PILDORAS INFORMATICAS
+#Python tutorizado. Archivos externos III. Vídeo 62
+
+import os #Importamos el módulo os para trabajar con archivos y directorios
+
+os.makedirs("PruebaDirectorio") #Creamos un nuevo directorio llamado PruebaDirectorio
