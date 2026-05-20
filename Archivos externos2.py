eb61b1d311ccd84fbcfc5865ff836388d7676a0e
Horacio Molinari <molinarih2011@gmail.com>
2026-05-20 12:29:10 -0300

    nuevos

--- Archivos externos2.py
+++ Archivos externos2.py
@@ -2,5 +2,15 @@
 #Python tutorizado. Archivos externos III. Vídeo 62
 
 import os #Importamos el módulo os para trabajar con archivos y directorios
+import io #Importamos el módulo io para trabajar con archivos de texto
 
-os.makedirs("PruebaDirectorio") #Creamos un nuevo directorio llamado PruebaDirectorio
+#os.makedirs("PruebaDirectorio") #Creamos un nuevo directorio llamado PruebaDirectorio
+
+#os.chdir("PruebaDirectorio") #Cambiamos el directorio de trabajo actual a PruebaDirectorio
+
+archivo_externo = open("ArchivoExterno.txt", "w") #Abrimos un archivo externo en modo escritura
+archivo_externo.write("Hola, soy un archivo externo") #Escribimos una línea de texto en el archivo
+
+print(os.getcwd()) #Imprimimos el directorio de trabajo actual
+
+print(os.listdir("./")) #Imprimimos la lista de archivos en el directorio actual
